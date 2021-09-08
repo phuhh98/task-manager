12846764d0b91e99b1ab0f987eba4ee9e2dfccbb
phuhh98 <phuhh98@gmail.com>
2021-09-08 21:56:34 +0700

    add router path to handler upload/delete/serve user avatar

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-require('dotenv').config(); //load .env to environment variables
-require('./db/mongoose.js'); //establish connect with mongodb
-const express = require('express');
-const chalk = require('chalk');
-const fs = require('fs');
-const path = require('path');
+require("dotenv").config(); //load .env to environment variables
+require("./db/mongoose.js"); //establish connect with mongodb
+const express = require("express");
+const chalk = require("chalk");
+const fs = require("fs");
+const path = require("path");
 
 const app = express(); // create a new server instance
 const PORT = process.env.PORT || 3000;
 
-const userRouter = require('./routers/user.js');
-const taskRouter = require('./routers/task.js');
-const auth = require('./middleware/auth.js');
-const morgan = require('morgan');
+const userRouter = require("./routers/user.js");
+const taskRouter = require("./routers/task.js");
+//const auth = require("./middleware/auth.js");
+const morgan = require("morgan");
 //Middlewares
 
 //maintainance
@@ -20,15 +20,32 @@ const morgan = require('morgan');
 // });
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json()); //middleware to parse body to json
-
 // create a write stream (in append mode)
-var accessLogStream = fs.createWriteStream(path.join(__dirname, 'access.log'), { flags: 'a' });
+var accessLogStream = fs.createWriteStream(path.join(__dirname, "access.log"), { flags: "a" });
 
 //Routers
 // setup the logger
-app.use(morgan('combined', { stream: accessLogStream }));
+app.use(morgan("combined", { stream: accessLogStream }));
 app.use(userRouter);
 app.use(taskRouter);
+
+function errorEmit(req, res, next) {
+	throw new Error("throw an error");
+}
+
+app.get(
+	"/upload",
+	errorEmit,
+	(req, res) => {
+		res.send("OK");
+	},
+	// error middleware at the end of middleware list to handler any error thrown
+	(err, req, res, next) => {
+		if (err) {
+			res.status(500).send(err.message);
+		}
+	}
+);
 
 app.listen(PORT, () => {
 	console.log(`Server started on port ${chalk.yellow(PORT)}`);
--- src/routers/user.js
+++ src/routers/user.js
@@ -1,128 +1,109 @@
-const { Router } = require('express');
-const User = require('../models/user.js'); //load User model
-const auth = require('../middleware/auth.js');
-const multer = require('multer'); // load multer for file uploading
-
-const { customAlphabet } = require('nanoid');
-const { alphanumeric } = require('nanoid-dictionary');
+const { Router } = require("express");
+const User = require("../models/user.js"); //load User model
+const auth = require("../middleware/auth.js");
+const multer = require("multer"); // load multer for file uploading
+const { customAlphabet } = require("nanoid");
+const { alphanumeric } = require("nanoid-dictionary");
 const alphanumericRandom = customAlphabet(alphanumeric, 10);
+const FileType = require("file-type"); // check file type
 const router = new Router();
 
 //set up multer upload image handler
 const uploadImage = multer({
-	storage: multer.diskStorage({
-		destination: function (req, file, callback) {
-			callback(null, 'images');
-		},
-		filename: async function (req, file, callback) {
-			//cb(err Error, filename<string>)
-			try {
-				const mimetype = file.mimetype.match(/^image\/(jpg|png|jpeg)$/)[1];
-				const name = alphanumericRandom();
-				console.log(mimetype, name);
-				return callback(null, `${file.fieldname}${name}.${mimetype}`);
-			} catch (err) {
-				callback(err);
-			}
-		},
-	}),
+	// comment the storage section of multer setting to let multer cache file buffer so that we can use to store in mongodb
+	// storage: multer.diskStorage({
+	// 	destination: function (req, file, callback) {
+	// 		callback(null, "images");
+	// 	},
+	// 	filename: async function (req, file, callback) {
+	// 		//cb(err Error, filename<string>)
+	// 		try {
+	// 			const mimetype = file.mimetype.match(/^image\/(jpg|png|jpeg)$/)[1];
+	// 			const name = alphanumericRandom();
+	// 			console.log(mimetype, name);
+	// 			callback(null, `${file.fieldname}${name}.${mimetype}`);
+	// 		} catch (err) {
+	// 			callback(err);
+	// 		}
+	// 	},
+	// }),
 
 	limits: {
 		fileSize: 1024 * 1024, //size in bytes
 	},
 
 	fileFilter(req, file, callback) {
-		//const mimeTypes = ['image/jpeg', 'image/png'];
+		const mimeTypes = ["image/jpeg", "image/png"];
 		if (!mimeTypes.includes(file.mimetype)) {
-			return cb(new multer.MulterError('Please upload a jpg|jpeg|png file'));
+			return callback(new multer.MulterError("Please upload a jpg|jpeg|png file"));
 			//have to have a return before cb(Error) to prevent file upload when not met filter
 		}
-
 		callback(null, true); // accepted
 	},
 });
 
-const uploadDoc = multer({
-	//set up multer upload handler
-	dest: 'images',
-	limit: {
-		fileSize: 1024 * 1024, //size in bytes
-	},
-	fileFilter(req, file, cb) {
-		console.log(file.mimetype);
-		if (!file.originalname.match(/\.(doc|docx)$/)) {
-			return cb(new multer.MulterError('Please upload a doc|docx file'));
-			//have to have a return before cb(Error) to prevent file upload when not met filter
-		}
-
-		cb(null, true); // accepted
-		// cb(null, false); // rejected with no error
-	},
-});
-
 //Create a new user, no authentication required
-router.post('/users', async (req, res) => {
-	const user = new User(req.body);
-
+router.post("/users", async (req, res) => {
 	try {
+		const user = new User(req.body);
 		await user.save();
 		const token = await user.generateAuthToken();
-		return res.status(201).send({ user, token });
+		return res.status(200).send({ user, token });
 	} catch (err) {
-		return res.status(400).send(err);
+		res.status(400).send(err);
 	}
 });
 
 //Login with incoming post req of user's email and password JSON
-router.post('/users/login', async (req, res) => {
+router.post("/users/login", async (req, res) => {
 	try {
 		const user = await User.findByCredentials(req.body.email, req.body.password);
 		const token = await user.generateAuthToken();
 
 		res.send({ user, token });
 	} catch (err) {
-		res.status(400).send('Wrong credentials');
+		res.status(400).send("Wrong credentials");
 	}
 });
 
 //Logout a session and delete an auth token from user
-router.post('/users/logout', auth, async (req, res) => {
+router.post("/users/logout", auth, async (req, res) => {
 	try {
 		//filter out the current token sent from client and remove it from user tokens <<array>>
 		req.user.tokens = req.user.tokens.filter((token) => {
 			return token.token !== req.token; // req.token is token carrier passed by auth <<middleware>>
 		});
 		await req.user.save();
-		res.status(200).send('Logout session succeed!');
+		res.status(200).send("Logout session succeed!");
 	} catch (err) {
 		res.status(400).send();
 	}
 });
 
 //Logout all sessions, drop user.tokens[]
-router.post('/users/logoutAll', auth, async (req, res) => {
+router.post("/users/logoutAll", auth, async (req, res) => {
 	try {
 		req.user.tokens = [];
 		await req.user.save();
-		res.status(200).send('Logout all sessions succeed!');
+		res.status(200).send("Logout all sessions succeed!");
 	} catch (err) {
-		res.status(400).send('Bad request');
+		res.status(400).send("Bad request");
 	}
 });
 
 //Get requesting user profile
-router.get('/users/me', auth, async (req, res) => {
+router.get("/users/me", auth, async (req, res) => {
 	res.send(req.user);
 });
 
 //Update user profile with authorized token
-router.patch('/users/me', auth, async (req, res) => {
+router.patch("/users/me", auth, async (req, res) => {
 	const updates = Object.keys(req.body); //extract keys on request json
-	const allowUpdates = ['name', 'email', 'age', 'password'];
+	const allowUpdates = ["name", "email", "age", "password"];
 	const isValidOperation = updates.every((update) => allowUpdates.includes(update));
 
 	if (!isValidOperation) {
-		return res.status(400).send({ error: 'Invalid updates!' });
+		return res.status(400).send({ error: "Invalid updates!" });
 	}
 
 	try {
@@ -135,27 +116,66 @@ router.patch('/users/me', auth, async (req, res) => {
 });
 
 //Delete a user from db with authorized token
-router.delete('/users/me', auth, async (req, res) => {
+router.delete("/users/me", auth, async (req, res) => {
 	try {
 		await req.user.remove();
-		res.status(200).send('User deleted');
+		res.status(200).send("User deleted");
 	} catch (err) {
 		res.status(500).send(err);
 	}
 });
 
 //Upload user avatar
-// upload a single file, binary contained inside req.file.avatar
-router.post('/user/me/avatar', (req, res) => {
-	uploadImage.single('avatar')(req, res, (err) => {
+// upload a single file, binary contained inside req.file.avatar after handled by auth() middleware
+router.post(
+	"/user/me/avatar",
+	auth,
+	uploadImage.single("avatar"), //multer middleware handles multipart file data and cache data in req.file
+	//actual request , response handler before error handler being set
+	async (req, res) => {
+		req.user.avatar = req.file.buffer; //set avatar field of current user which is uploading avatar to that binary one.
+		await req.user.save();
+		res.status(200).send("File uploaded");
+	},
+	//error handler in expressjs style
+	(err, req, res, next) => {
 		if (err instanceof multer.MulterError) {
 			res.status(400).send(err);
 		} else if (err) {
-			res.status(500).send('Interal server error!');
+			res.status(500).send("Interal server error!");
 		} else {
-			res.status(200).send('File uploaded');
+			console.log("ok");
 		}
-	});
+	}
+);
+
+router.delete(
+	"/user/me/avatar",
+	auth,
+	async (req, res) => {
+		req.user.avatar = undefined;
+		await req.user.save();
+		res.status(200).send("Avatar deleted");
+	},
+	(err, req, res, next) => {
+		if (err) {
+			res.status(400).send(err);
+		}
+	}
+);
+
+router.get("/user/:id/avatar", async (req, res) => {
+	try {
+		const user = await User.findOne({ _id: req.params.id });
+		if (!user || !user.avatar) {
+			throw new Error();
+		}
+		const filetype = await FileType.fromBuffer(user.avatar);
+		res.set("Content-Type", `${filetype.mime}`); // need to set Content-Type header to let browser regconize what it's received
+		res.status(200).send(user.avatar);
+	} catch (err) {
+		res.status(404).send();
+	}
 });
 
 module.exports = router;
